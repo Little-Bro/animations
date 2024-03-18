181584a4dfe1a9b3f1bce8eed0369a583915da65
Little-Bro <tom.ericdelmas@gmail.com>
2024-03-18 12:49:44 +0100

    nouvelle animations ions dans la partie chimie

---
+++ ions/atome.js
@@ -0,0 +1,87 @@
+class Atome {
+	constructor(protons, neutrons) {
+    let offset = 15;
+    this.isAtom = true;
+		this.protons = protons;
+		this.neutrons = neutrons;
+    this.electrons = this.protons;
+    this.protonsPos = [];
+    this.neutronsPos = [];
+    this.electronsPos = [];
+    this.thetas = [];
+    
+    // protons
+    for (let i = 0; i < this.protons; i++) {
+			let pos = createVector(width/2 + random(offset), height/2 + random(offset));
+      this.protonsPos.push(pos);
+    }
+    
+    // neutrons
+    for (let i = 0; i < this.neutrons; i++) {
+			let pos = createVector(width/2 + random(offset), height/2 + random(offset));
+      this.neutronsPos.push(pos);
+    }
+    
+    // electrons
+    for (let i = 0; i < this.protons; i++) {
+      this.thetas.push(random(0, TWO_PI));
+    	let pos = createVector(width/2 + random(100, 250), height/2 + random(100, 250));
+      this.electronsPos.push(pos);
+    }
+  }
+  
+  show() {
+    //neutrons
+   	for (let i = 0; i < this.neutrons; i++) {
+		  fill(0, 255, 0);
+      circle(this.neutronsPos[i].x, this.neutronsPos[i].y, 15);
+      for (let j = 0; j < i; j++) {
+				let d = dist(this.neutronsPos[i].x, this.neutronsPos[i].y, this.neutronsPos[j].x, this.neutronsPos[j].y)
+        if (d < 10) {
+        	this.neutronsPos[i].add(p5.Vector.random2D());
+        }
+      }
+    }
+    // protons
+    for (let i = 0; i < this.protons; i++) {
+			fill(255, 0, 0);
+      circle(this.protonsPos[i].x, this.protonsPos[i].y, 15);
+      line(this.protonsPos[i].x - 4, this.protonsPos[i].y, this.protonsPos[i].x + 4, this.protonsPos[i].y)
+      line(this.protonsPos[i].x, this.protonsPos[i].y - 4, this.protonsPos[i].x, this.protonsPos[i].y + 4)
+      for (let j = 0; j < i; j++) {
+				let d = dist(this.protonsPos[i].x, this.protonsPos[i].y, this.protonsPos[j].x, this.protonsPos[j].y)
+        if (d < 15) {
+        	this.protonsPos[i].add(p5.Vector.random2D());
+        }
+      }
+    }
+    
+    // électrons
+    for (let i = 0; i < this.electrons; i++) {
+    	fill(0, 0, 255);
+      let x1 = this.electronsPos[i].x;
+      let y1 = this.electronsPos[i].y;
+      let x2 = width/2;
+      let y2 = height/2;
+      let r = Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
+      let x = width/2 + r * cos(this.thetas[i]);
+      let y = height/2 + r * sin(this.thetas[i]);
+      circle(x, y, 10);
+      line(x - 4, y, x + 4, y)
+    }
+  }
+  
+  ionise(number, type) {
+    if (type == 'cation')
+			this.electrons -= number;
+    else if (type == 'anion') {
+      this.electrons += number
+			for (let i = 0; i < number; i++) {
+        this.thetas.push(random(0, TWO_PI));
+        let pos = createVector(width/2 + random(100, 250), height/2 + random(100, 250));
+        this.electronsPos.push(pos);
+    	}
+    }
+    ;
+  }
+}
---
+++ ions/sketch.js
@@ -0,0 +1,74 @@
+// TODO
+/*
+	ajouter des éléments
+  avoir un noyeau plus joli
+*/
+
+let particule;
+let nature;
+let theta = 0;
+let ionisationBouton;
+
+function setup() {
+	createCanvas(700, 700);
+  particule = new Atome(11, 12);
+  ionisationBouton = createButton('Ionisation');
+  ionisationBouton.mousePressed(() => ionisation());
+}
+function draw() {
+  background(255);
+  noFill();
+  rect(0, 0, width, height);
+  for (let i = 0; i < particule.thetas.length; i++) {
+		particule.thetas[i] += PI/200;
+  }
+ 	particule.show();
+  
+  // TEXTE EN HAUT A GAUCHE
+ 	fill(0);
+  textSize(24);
+  textStyle(NORMAL);
+  text('J\'ai', 10, 30);
+  fill(255, 0, 0);
+  text(`${particule.protons} ${particule.protons > 1 ? 'protons' : 'proton'}`, 55, 30);
+  fill(0);
+  text('et', 175, 30);
+  fill(0, 0, 255);
+  text(`${particule.electrons} ${particule.electrons > 1 ? 'électrons' : 'électron'}`, 205, 30);
+  fill(0);
+  text('je suis un', 340, 30);
+  natureTexte = parseInt(particule.protons) == parseInt(particule.electrons) ? 'atome' : 'ion';
+	textStyle(BOLD);
+  text(natureTexte, 445, 30);
+  textStyle(NORMAL);
+  chargeElec = parseInt(particule.protons) - parseInt(particule.electrons);
+  text(`Charge électrique :`, 10, 70);
+ 	let charge = chargeElec.toString().split("").reverse().join("") ;
+  if (chargeElec > 0) {
+	  fill(255, 0, 0);
+  }
+  else if (chargeElec < 0)
+    fill(0, 0, 255);
+  else 
+    fill(0);
+  
+  text(`${chargeElec}`, 225, 70);
+  if (chargeElec > 0)
+    text('+', 250, 70);
+  fill(0);
+  
+  // TEXTE EN BAS A DROITE
+  textSize(30);
+  text(natureTexte + '   Na', 500, 675);
+  textSize(16);
+  if (natureTexte == 'atome') {
+		text(particule.protons, 588, 680);
+		text(particule.protons + particule.neutrons, 588, 660);
+  } else {
+		text(charge + `${chargeElec > 0 ? '+' : ''}`, 605, 665);
+  }
+}
+  
+function ionisation() {
+	particule.ionise(2, 'cation');
+}
